dcc95b54936a35d601812b16a706374c47f5df71
luizkraisch <luizkraisch22@gmail.com>
2026-04-08 00:03:32 -0300

    feat: add theme switcher, settings page, shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:moodoo/app_theme.dart' show lightTheme, darkTheme;
 import 'package:moodoo/pages/home_page.dart';
+import 'package:moodoo/theme_preferences.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await loadTheme();
   runApp(const MoodooApp());
 }
 
@@ -11,13 +14,18 @@ class MoodooApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Moodoo App',
-      debugShowCheckedModeBanner: false,
-      theme: lightTheme,
-      darkTheme: darkTheme,
-      themeMode: ThemeMode.system,
-      home: const HomePage(),
+    return ValueListenableBuilder<ThemeMode>(
+      valueListenable: themeModeNotifier,
+      builder: (context, themeMode, _) {
+        return MaterialApp(
+          title: 'Moodoo App',
+          debugShowCheckedModeBanner: false,
+          theme: lightTheme,
+          darkTheme: darkTheme,
+          themeMode: themeMode,
+          home: const HomePage(),
+        );
+      },
     );
   }
 }
