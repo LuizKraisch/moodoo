c01980c2ae2a20130407518a0fb715a4df45a90a
luizkraisch <luizkraisch22@gmail.com>
2026-04-15 22:34:44 -0300

    fix: update spec to support firebase integration

--- lib/pages/auth_gate.dart
+++ lib/pages/auth_gate.dart
@@ -4,13 +4,14 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class AuthGate extends StatelessWidget {
-  const AuthGate({super.key});
+  final Stream<User?>? authStream;
+  const AuthGate({super.key, this.authStream});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
+        stream: authStream ?? FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             return HomePage();
