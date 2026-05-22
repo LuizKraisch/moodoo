eb861e00f7aa897558ae02a765c7fd31b8bcab73
luizkraisch <luizkraisch22@gmail.com>
2026-05-22 00:26:14 -0300

    feat: adjust offset in LoginPagePresentation for improved layout

--- lib/widgets/login_page_presentation.dart
+++ lib/widgets/login_page_presentation.dart
@@ -32,7 +32,7 @@ class LoginPagePresentation extends StatelessWidget {
       child: LayoutBuilder(
         builder: (context, constraints) => ClipRect(
           child: Transform.translate(
-            offset: const Offset(-60, 170),
+            offset: const Offset(-60, 145),
             child: OverflowBox(
               maxWidth: double.infinity,
               maxHeight: double.infinity,
